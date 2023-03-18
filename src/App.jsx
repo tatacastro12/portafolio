906df81f5b378cc9e1dc9d4f36ae78096f760485
tatacastro12 <91699554+tatacastro12@users.noreply.github.com>
2023-03-18 11:12:11 -0700

    Portafolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import About from "./components/About";
 import Footer from "./components/Footer";
-import Form from "./components/form";
 import HeroSection from "./components/HeroSection";
 import Services from "./components/Services";
 import Works from "./components/Works";
+import Form from  "./components/Form"
 
 function App() {
   const [theme, setTheme] = useState(null);
